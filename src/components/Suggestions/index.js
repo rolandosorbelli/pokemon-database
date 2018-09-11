238381d84703aec61984ebaa436b08e38945dae3
rolandosorbelli <rolando.sorbelli@bbc.co.uk>
2018-09-11 10:57:40 +0100

    Fix display bug in dungeons/raid description

--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -5,30 +5,42 @@ const Suggestions = (props) => {
     <div className="suggestions">
       {props.results.map(item => {
         /*
-          Check if it's a dungeon or a raid
+        Check if it's a dungeon or a raid
         */
         let type
         (item.dungeon ? type = 'Dungeon' : type = 'Raid')
 
         /*
-          Check number of players
+        Check number of players
         */
         let players
         (item.numPlayers === '10/25' ? players = '10 or 25' : players = item.numPlayers)
         let extraInfo
-        (item.description === "" ? extraInfo = "No description available" : extraInfo = '...')
+        (item.description === '' ? extraInfo = 'No description available' : extraInfo = '')
 
         return (
           <div key={item.id} className="suggestions--card">
-            <div className="suggestions--card__header">
+            <div className="suggestions--card--header">
               <p>{type}</p>
               <p>See more</p>
             </div>
-            <div className="suggestions--card__content">
-              <p>Name: {item.name}</p>
-              <p>Location: {item.location}</p>
-              <p>Number of players: {players}</p>
-              <p>Info: {item.description.substring(0,50)}{extraInfo}</p>
+            <div className="suggestions--card--content">
+              <div className="suggestions--card--content--section">
+                <p className="suggestions--card--content--title">Name</p>
+                <p>{item.name}</p>
+              </div>
+              <div className="suggestions--card--content--section">
+                <p className="suggestions--card--content--title">Location</p>
+                <p>{item.location}</p>
+              </div>
+              <div className="suggestions--card--content--section">
+                <p className="suggestions--card--content--title">Number of players</p>
+                <p>{players}</p>
+              </div>
+              <div className="suggestions--card--content--section">
+                <p className="suggestions--card--content--title">Info</p>
+                <p>{item.description}{extraInfo}</p>
+              </div>
             </div>
           </div>
         )
